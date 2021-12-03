27a2aa84217e9652c144366c74beb4d1c4afe97a
Rohith Remesan <rohithremesan507@gmail.com>
2021-12-03 16:03:41 +0530

    logi,registration

---
+++ app/src/main/java/com/example/tinder/ChooseLoginRegistrationActivity.java
@@ -0,0 +1,35 @@
+package com.example.tinder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class ChooseLoginRegistrationActivity extends AppCompatActivity {
+
+private Button mLogin,mRegister;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_login_registration);
+        mLogin = (Button) findViewById(R.id.login);
+        mRegister = (Button) findViewById(R.id.register);
+
+        mLogin.setOnClickListener((view) -> {
+            Intent intent = new Intent(ChooseLoginRegistrationActivity.this,LoginActivity.class);
+            startActivity(intent);
+            finish();
+            return;
+        });
+
+        mRegister.setOnClickListener((view) -> {
+         Intent intent = new Intent(ChooseLoginRegistrationActivity.this,RegistrationActivity.class);
+         startActivity(intent);
+         finish();
+         return;
+        });
+    }
+}
